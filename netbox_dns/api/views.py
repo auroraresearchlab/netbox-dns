345303c0a3dc28b06a3678bafae01df7712b82d8
Süleyman ERGEN <suleyman.ergen@aurorabilisim.com>
2021-08-12 14:33:47 +0300

    record api implemented

--- netbox_dns/api/views.py
+++ netbox_dns/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from netbox_dns.models import Zone, NameServer
-from netbox_dns.api.serializers import NameServerSerializer, ZoneSerializer
-from netbox_dns.filters import NameServerFilter, ZoneFilter
+from netbox_dns.models import Zone, NameServer, Record
+from netbox_dns.api.serializers import ZoneSerializer, NameServerSerializer, RecordSerializer
+from netbox_dns.filters import ZoneFilter, NameServerFilter, RecordFilter
 
 
 class ZoneViewSet(ModelViewSet):
@@ -10,8 +12,26 @@ class ZoneViewSet(ModelViewSet):
     serializer_class = ZoneSerializer
     filterset_class = ZoneFilter
 
+    @action(detail=True, methods=['get'])
+    def records(self, request, pk=None):
+        records = Record.objects.filter(zone=pk)
+        serializer = RecordSerializer(records, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'])
+    def nameservers(self, request, pk=None):
+        nameservers = NameServer.objects.filter(zones__id=pk)
+        serializer = NameServerSerializer(nameservers, many=True, context={'request': request})
+        return Response(serializer.data)
+
 
 class NameServerViewSet(ModelViewSet):
     queryset = NameServer.objects.all()
     serializer_class = NameServerSerializer
     filterset_class = NameServerFilter
+
+
+class RecordViewSet(ModelViewSet):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+    filterset_class = RecordFilter
