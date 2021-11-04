259d6c9eeca79dfc525728c515794b309938c0d1
Süleyman ERGEN <suleyman.ergen@aurorabilisim.com>
2021-11-04 17:14:15 +0300

    fix black formatting

--- netbox_dns/migrations/0003_zone_new_fields.py
+++ netbox_dns/migrations/0003_zone_new_fields.py
@@ -7,34 +7,40 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tenancy', '0002_tenant_ordering'),
-        ('netbox_dns', '0002_zone_default_ttl'),
+        ("tenancy", "0002_tenant_ordering"),
+        ("netbox_dns", "0002_zone_default_ttl"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='zone',
-            name='auto_renew',
+            model_name="zone",
+            name="auto_renew",
             field=models.BooleanField(default=False, null=True),
         ),
         migrations.AddField(
-            model_name='zone',
-            name='comments',
+            model_name="zone",
+            name="comments",
             field=models.TextField(blank=True),
         ),
         migrations.AddField(
-            model_name='zone',
-            name='expire_date',
+            model_name="zone",
+            name="expire_date",
             field=models.DateField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='zone',
-            name='ssl_expire_date',
+            model_name="zone",
+            name="ssl_expire_date",
             field=models.DateField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='zone',
-            name='tenant',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='zones', to='tenancy.tenant'),
+            model_name="zone",
+            name="tenant",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="zones",
+                to="tenancy.tenant",
+            ),
         ),
     ]
