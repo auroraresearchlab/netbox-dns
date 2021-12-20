e4a128813951c4671551833fcfa497eb73a72b43
Peter Eckel <pe-git@hindenburgring.com>
2021-12-20 19:11:44 +0100

    Execute migration 0005 after 0007

--- netbox_dns/migrations/0006_zone_soa_serial_auto.py
+++ netbox_dns/migrations/0006_zone_soa_serial_auto.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("netbox_dns", "0005_update_ns_records"),
+        ("netbox_dns", "0004_create_ptr_for_a_aaaa_records")
     ]
 
     operations = [
