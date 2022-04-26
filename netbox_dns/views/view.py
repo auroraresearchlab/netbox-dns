bb0c70a33261f1bf6938447783838bcc9e27bcd0
Peter Eckel <pe-git@hindenburgring.com>
2022-04-26 18:36:41 +0200

    Provided zone list in View view

--- netbox_dns/views/view.py
+++ netbox_dns/views/view.py
@@ -1,13 +1,33 @@
 from netbox.views import generic
 
-from netbox_dns.models import View
+from netbox_dns.models import View, Zone
 from netbox_dns.filters import ViewFilter
 from netbox_dns.forms import ViewForm, ViewFilterForm, ViewCSVForm, ViewBulkEditForm
-from netbox_dns.tables import ViewTable
+from netbox_dns.tables import ViewTable, ZoneTable
 
 
 class ViewView(generic.ObjectView):
     queryset = View.objects.all()
+
+    def get_extra_context(self, request, instance):
+        zones = instance.zone_set.all()
+        zone_table = ZoneTable(list(zones), user=request.user)
+
+        change_zone = request.user.has_perm("netbox_dns.change_zone")
+        delete_zone = request.user.has_perm("netbox_dns.delete_zone")
+
+        if change_zone or delete_zone:
+            zone_table.columns.show("pk")
+        zone_table.configure(request)
+
+        return {
+            "zone_table": zone_table,
+            "permissions": {
+                "change": change_zone,
+                "delete": delete_zone,
+            },
+            "model": Zone,
+        }
 
 
 class ViewListView(generic.ObjectListView):
