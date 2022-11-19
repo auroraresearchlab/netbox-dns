fe5b65bcb2a53445d28fa724a1aac7401626342d
Peter Eckel <pe-git@hindenburgring.com>
2022-11-19 16:40:19 +0100

    Modified all tests to succeed with NetBox 3.4-beta1

--- netbox_dns/tests/nameserver/test_views.py
+++ netbox_dns/tests/nameserver/test_views.py
@@ -20,13 +20,13 @@ class NameServerTestCase(
 
     @classmethod
     def setUpTestData(cls):
-        NameServer.objects.bulk_create(
-            [
-                NameServer(name="ns1.example.com"),
-                NameServer(name="ns2.example.com"),
-                NameServer(name="ns3.example.com"),
-            ]
+
+        cls.nameservers = (
+            NameServer(name="ns1.example.com"),
+            NameServer(name="ns2.example.com"),
+            NameServer(name="ns3.example.com"),
         )
+        NameServer.objects.bulk_create(cls.nameservers)
 
         tags = create_tags("Alpha", "Bravo", "Charlie")
 
@@ -40,6 +40,13 @@ class NameServerTestCase(
             "ns5.example.com",
             "ns6.example.com",
             "ns7.example.com",
+        )
+
+        cls.csv_update_data = (
+            "id,name,description",
+            f"{cls.nameservers[0].pk},ns8.hindenburgring.com,test1",
+            f"{cls.nameservers[1].pk},ns9.hindenburgring.com,test2",
+            f"{cls.nameservers[2].pk},ns10.hindenburgring.com,test3",
         )
 
     maxDiff = None
