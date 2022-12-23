412d730ddaefb22abfb8b098a2fe11a806885460
Peter Eckel <pe-git@hindenburgring.com>
2022-12-23 14:27:16 +0100

    Provide the option to tolerate underscores in hostnames when validating

--- netbox_dns/tests/zone/test_name_validation.py
+++ netbox_dns/tests/zone/test_name_validation.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.core.exceptions import ValidationError
 
 from netbox_dns.models import NameServer, Zone
@@ -26,6 +26,8 @@ class NameValidationTest(TestCase):
         names = (
             "zone1.example.com",
             "zone2.example.com.",
+            "zone-1.example.com",  # dash in first label
+            "zone1.example-1.com",  # dash in second label
             "1234567" + ".12345678" * 26 + ".example.com",  # 254 octets
             "1234567" + ".12345678" * 26 + ".example.com.",  # 255 octets, trailing dot
             "x" * 63 + ".example.com",  # longest label 63 octets
@@ -40,7 +42,48 @@ class NameValidationTest(TestCase):
 
     def test_name_validation_failure(self):
         names = (
-            "ns1..example.com",  # empty label
+            "-zone.example.com",  # leading dash in first label
+            "zone1.-example.com",  # leading dash in second label
+            "zone1.example.com-1",  # dash in TLD
+            "zone1..example.com",  # empty label
+            "-zone1..example.com-1",  # leading dash
+            "x" * 64 + ".example.com",  # label too long
+            "12345678" + ".12345678" * 26 + ".example.com",  # 255 octets
+            "123456789"
+            + ".12345678" * 26
+            + ".example.com.",  # 256 octets, trailing dot
+        )
+
+        for name in names:
+            with self.assertRaises(ValidationError):
+                Zone.objects.create(
+                    name=name, **self.zone_data, soa_mname=self.nameserver
+                )
+
+    @override_settings(
+        PLUGINS_CONFIG={"netbox_dns": {"tolerate_underscores_in_hostnames": True}}
+    )
+    def test_name_validation_tolerant_ok(self):
+        names = (
+            "zone_1.example.com",  # underscore in first label
+            "zone1.example_1.com",  # underscore in second label
+        )
+
+        for name in names:
+            zone = Zone.objects.create(
+                name=name, **self.zone_data, soa_mname=self.nameserver
+            )
+            self.assertEqual(zone.name, name)
+
+    @override_settings(
+        PLUGINS_CONFIG={"netbox_dns": {"tolerate_underscores_in_hostnames": True}}
+    )
+    def test_name_validation_tolerant_failure(self):
+        names = (
+            "_zone1.example.com",  # leading underscore in first label
+            "zone1._example.com",  # leading underscore in second label
+            "zone1.example.com_1",  # underscore in TLD
+            "zone1..example.com",  # empty label
             "x" * 64 + ".example.com",  # label too long
             "12345678" + ".12345678" * 26 + ".example.com",  # 255 octets
             "123456789" + ".12345678" * 26 + ".example.com",  # 256 octets, trailing dot
