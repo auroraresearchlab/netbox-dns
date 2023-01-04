0171c4f492b9ca1ecab7f480d375f3c641ce483b
Peter Eckel <pe-git@hindenburgring.com>
2023-01-04 17:02:11 +0100

    Created a data migration to re-index NetBox DNS models search index

---
+++ netbox_dns/migrations/0022_search.py
@@ -0,0 +1,30 @@
+import sys
+
+from packaging import version
+
+from django.core import management
+from django.db import migrations
+from django.conf import settings
+
+
+def reindex(apps, schema_editor):
+    if "test" not in sys.argv:
+        if version.parse(settings.VERSION) >= version.parse("3.4.2"):
+            management.call_command("reindex", "netbox_dns")
+        else:
+            management.call_command("reindex", "netbox_dns.view")
+            management.call_command("reindex", "netbox_dns.nameserver")
+            management.call_command("reindex", "netbox_dns.zone")
+            management.call_command("reindex", "netbox_dns.record")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("extras", "0083_search"),
+        ("netbox_dns", "0021_record_ip_address"),
+    ]
+
+    operations = [
+        migrations.RunPython(code=reindex, reverse_code=migrations.RunPython.noop),
+    ]
